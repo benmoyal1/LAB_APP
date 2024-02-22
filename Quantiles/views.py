85b7c7c7e753e789e3274f12b72ce6939f2a7a2a
benmoyal1 <benmoyal2013@gmail.com>
2024-02-22 23:59:03 +0200

    Fucntionality added

--- Quantiles/views.py
+++ Quantiles/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render
 from django.views import View
+from django.shortcuts import render
+
 
 class QuantilesCalcView(View):
+    template_name = 'QuantileCalc.html'
+
     def get(self, request, *args, **kwargs):
-        return render(request, 'QuantileCalc.html')
+        # Get the numeric parameters from the URL
+        param1 = float(request.GET.get('param1', 0))
+        param2 = float(request.GET.get('param2', 0))
+
+        # Perform the calculation (you can replace this with your actual calculation logic)
+        result = param1 + param2
+
+        # Pass the result to the template
+        context = {'result': result}
+
+        return render(request, self.template_name, context)
