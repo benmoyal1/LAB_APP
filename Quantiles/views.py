9a4d398da531f0d3c936b1451340d42a546779d7
benmoyal1 <benmoyal2013@gmail.com>
2024-02-22 23:48:47 +0200

    Deployment HTMl

--- Quantiles/views.py
+++ Quantiles/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
-class QuantilesCalcView:
-    def get(self,request,*args,**kwargs):
-        return render(request,'QuantileCalc.html')
+from django.views import View
+
+class QuantilesCalcView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'QuantileCalc.html')
