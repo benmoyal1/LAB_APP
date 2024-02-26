73bd842aec20e9747b1aa34aafc75e01f13f2ac4
benmoyal1 <benmoyal2013@gmail.com>
2024-02-26 21:53:29 +0200

    changed polynomial func

--- Quantiles/views.py
+++ Quantiles/views.py
@@ -1,8 +1,5 @@
 from django.views import View
 from django.shortcuts import render
-import os
-import pandas as pd
-import numpy as np
 from scipy.stats import norm
 
 
@@ -45,31 +42,6 @@ class QuantilesCalcView(View):
 
         return render(request, self.template_name, context)
 
-    def get_quantile_day(self, ab_cir, day, path, mult):
-        if (day < 15 or day > 40) and (mult == 7):
-            return "Please Provide a Week between 15 and 40"
-        if (day < 105 or day > 280) and (mult == 1):
-            return "Please Provide a day between 105 and 280"
-        day_ = float(int(day * mult))
-        df = pd.read_csv(path)
-        df['day'] = df['pregweek'] * 7
-        df.drop('median', inplace=True, axis=1)
-        print("day is ", day_)
-        print(df)
-        quantiles = df[df['day'] == day_]
-        if ab_cir < quantiles['Q3'].iloc[0]:
-            return "This baby's Abdominal circumference is lower than 97% of the babies"
-
-        elif ab_cir < quantiles['Q5'].iloc[0]:
-            return "This baby's Abdominal circumference is lower than 95% of the babies"
-        elif ab_cir < quantiles['Q10'].iloc[0]:
-            return "This baby's Abdominal circumference is lower than 90% of the babies"
-        elif quantiles['Q95'].iloc[0] <= ab_cir < quantiles['Q97.5'].iloc[0]:
-            return "This baby's Abdominal circumference is greater than  95% of the babies"
-        elif ab_cir >= quantiles['Q97.5'].iloc[0]:
-            return "This baby's Abdominal circumference is greater than  97.5% of the babies"
-        else:
-            return "This baby's result are Normal"
 
     def mean_poly(self, week):
         mean_coefs = [-0.00581, 0.40206, 1.84235, -4.5837]  # 0.0058X^3
